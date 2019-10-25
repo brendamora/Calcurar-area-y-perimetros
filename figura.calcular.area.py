6faafd4ea07fa1fe119c140310aa747429c7bf4b
brendamora <56617399+brendamora@users.noreply.github.com>
2019-10-24 21:04:13 -0500

    Add files via upload

---
+++ figura.calcular.area.py
@@ -0,0 +1,25 @@
+from figura import *
+
+p1 = Punto(40,8)
+p2 = Punto(50,10)
+
+triangulo = Triangulo(p1, p2)
+triangulo.calcular_perimetro()
+
+print (triangulo.perimetro)
+
+"""
+Cuadrado = cuadrado (p1, p2)
+Circulo = circulo (p1, p2)
+rectangulo = Rectangulo(p1, p2)
+triangulo = Triangulo(p1, p2)
+
+
+triangulo.calcular_area()
+rectangulo.calcular_area()
+cuadrado.calcular_area()
+circulo.calcular_area()
+
+print (cuadrado.area)
+print (circulo.area)""" 
+
